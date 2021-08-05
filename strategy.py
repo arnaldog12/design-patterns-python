bf914fbb3ce4435e3ad80c5b61e5f9720cfd2506
Arnaldo Gualberto <arnaldo.gualberto@wildlifestudios.com>
2021-08-05 17:33:08 -0300

    strategy pattern

---
+++ strategy.py
@@ -0,0 +1,43 @@
+from abc import abstractmethod
+
+
+class ShoppingCart:
+    def __init__(self, list_items):
+        self.list_items = list_items
+
+    def pay(self, payment_strategy):
+        payment_strategy.pay()
+
+
+# Strategy Pattern
+class PaymentStrategy:
+    @abstractmethod
+    def pay(self):
+        pass
+
+
+class CreditCart(PaymentStrategy):
+    def __init__(self, name, card_number, cvv, exp_date):
+        self.name = name
+        self.card_number = card_number
+        self.cvv = cvv
+        self.exp_date = exp_date
+
+    def pay(self):
+        print(f"paying with credit card nº: {self.card_number}")
+
+
+class PayPal(PaymentStrategy):
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def pay(self):
+        print(f"paying with paypal from user: {self.email}")
+
+
+cart = ShoppingCart(["item 1", "item 2", "item 3"])
+cart.pay(CreditCart("Arnaldo", "1111.2222.3333.4444", "000", "2100/01/01"))
+cart.pay(PayPal("arnaldo.gualberto@wildlifestudios.com", "1234"))
+
+
